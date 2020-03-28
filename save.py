ee34ec71f4f5b0e591b1173e4cf13a5e1e4df924
PecaLee <58648931+PecaLee@users.noreply.github.com>
2020-03-28 23:52:27 +0900

    encode

--- save.py
+++ save.py
@@ -1,7 +1,7 @@
 import csv
 
 def save_to_file(goods):
-    file = open("goods.csv", mode="w")
+    file = open("goods.csv", mode="w", encoding="utf-8")
     writer = csv.writer(file)
     writer.writerow(["title","price","link"])
     for good in goods:
